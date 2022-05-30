b855b8023652f028f3359a313cde04d692e68b5a
Nchenz <nuttawatj3@gmail.com>
2022-05-30 11:57:29 +0700

    createlightswitchPOP and OOP

---
+++ oop1/lightswitch_oop.py
@@ -0,0 +1,26 @@
+#ต้องสร้างclass = แม่พิมพ์ ตัวต้นแบบ
+#object = สิ่งที่ได้ออกมาจากแม่พิมพ์ ถูกสร้างมาจากแม่พิมพ์
+class LightSwitch():
+    def __init__(self) -> None: #เครื่องหมาย'_' สองครั้งทั้งหน้าและหลัง init
+        self.switch_status = False
+
+    def turnon(self):
+        self.switch_status = True
+
+    def turnoff(self):
+        self.switch_status = False
+
+    def show(self):
+        print(f'status = {self.switch_status}')
+
+#สร้างวัตถุ(object) จากแม่พิมพ์(class)
+switch_obj = LightSwitch() #switch_obj ถูกสร้างมาจากแม่พิมพ์LightSwitch
+
+#เรียกใช้method
+switch_obj.show() #False
+switch_obj.turnon()
+switch_obj.show() #True
+switch_obj.turnoff()
+switch_obj.show() #False
+switch_obj.turnoff()
+switch_obj.show() #False
---
+++ oop1/lightswitch_procedure.py
@@ -0,0 +1,21 @@
+#สร้างฟังก์ชั่นเปิดไฟ
+
+def turnon():
+    global switch_status
+    switch_status = True #เปลี่ยนสถานะเป็นเปิดไฟ
+
+
+#สร้างฟังก์ชั่นปิดไฟ
+def turnoff():
+    global switch_status
+    switch_status = False #เปลี่ยนสถานะเป็นปิดไฟ
+
+switch_status = False #ไฟปิด
+
+print(f'Status = {switch_status}') #False
+turnon()
+print(f'Status = {switch_status}') #True
+turnoff()
+print(f'Status = {switch_status}') #False
+turnoff()
+print(f'Status = {switch_status}') #False
