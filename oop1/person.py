e8200c68a943551950d97fbd62bc016f06125c82
Nchenz <nuttawatj3@gmail.com>
2022-06-14 13:05:47 +0700

    modify person

--- oop1/person.py
+++ oop1/person.py
@@ -1,5 +1,7 @@
 class  person:
+    country = "Thailand" #class variables
     def __init__(self,name,gender,profession,hours) -> None:
+        #instance variables
         self.name = name
         self.gender = gender
         self.profession = profession
@@ -30,3 +32,21 @@ jon.study()
 #สร้าง object ตัวใหม่
 lalisa = person("Lalisa","Female","Korea Singer","13")
 lalisa.work()
+
+#เรียกใช้ class variables
+print(f"Class Variables: {person.country}")
+print(f"Instance Variables: {lalisa.country}")
+
+#เปลี่ยนค่า เฉพาะ object
+lalisa.country = "Korea"
+print("--------")
+print(f"Class Variables: {person.country}")
+print(f"Instance Variables: {lalisa.country}")
+print(f"Instance Variables: {jon.country}")  #ไม่กระทบกับค่าของobject อื่นๆ
+
+#แก้class 
+person.country = "England"
+print("*******")
+print(f"Class Variables: {person.country}")
+print(f"Instance Variables: {lalisa.country}")
+print(f"Instance Variables: {jon.country}")
