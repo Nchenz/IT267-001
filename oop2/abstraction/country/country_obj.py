b44fbcb1c48de85f93a78c990fe09c61dad2a52b
Nchenz <nuttawatj3@gmail.com>
2022-07-04 14:20:37 +0700

    abstraction update

---
+++ oop2/abstraction/country/country_obj.py
@@ -0,0 +1,12 @@
+from country_subclass import *
+
+#create europe object
+eu = Europe('Norway',5505859)
+eu.location = '63.4772451,0.1104799'
+eu.capital = 'Oslo'
+eu.show_detail()
+
+#create asia object
+a= Asia('Singapore',5686543)
+a.gdp = 340
+a.show_detail()
